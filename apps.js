cb824c39e3802470db261d957c0e330ab85c1902
revelation136 <revelation136@gmail.com>
2022-03-03 08:33:23 +0800

    Learning how to use async and await and adding a function that reload the page every 5sec

---
+++ apps.js
@@ -0,0 +1,22 @@
+const api_url_iss = 'https://api.wheretheiss.at/v1/satellites/25544';
+
+// async - makes a function return Promise
+// await - makes a function wait for a Promise
+async function getAPI_DATA_ISS() {
+    const response = await fetch(api_url_iss);
+    const data = await response.json();
+
+    // Deconstructing in JS (Separating lat and lon to individual Variable)
+    const {latitude, longitude} = data;
+    console.log(data);
+    console.log(latitude);
+    console.log(longitude);
+
+    document.querySelector('#lat').textContent = latitude;
+    document.querySelector('#lon').textContent = longitude;
+}
+
+getAPI_DATA_ISS();
+
+// Reloading the page every 5 sec
+setInterval('window.location.reload()', 5000);
