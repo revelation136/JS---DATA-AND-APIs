fdf1196a081f8eb3aa925d0c0dc658dafb72ebd4
revelation136 <revelation136@gmail.com>
2022-03-22 15:21:41 +0800

    whereistheiss.js - mapping out the longitude and latitude with icon

--- whereistheiss.js
+++ whereistheiss.js
@@ -1,3 +1,23 @@
+// Map and Tiles
+// L means leaflet
+// setView([latitude, longitude]zoom level)
+const mymap = L.map('issMap').setView([0, 0], 1);
+// map is created but no tiles, attribution is required
+const attribution = 
+'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileURL, { attribution });
+tiles.addTo(mymap); 
+
+
+// Marker and Marker Icon
+const issIcon = L.icon({
+    iconUrl: 'iss-icon.png',
+    iconSize: [50, 32],
+    iconAnchoor: [25, 16]
+});
+const marker = L.marker([0, 0], { icon: issIcon }).addTo(mymap);
+
 const api_url_iss = 'https://api.wheretheiss.at/v1/satellites/25544';
 
 // async - makes a function return Promise
@@ -5,29 +25,32 @@ const api_url_iss = 'https://api.wheretheiss.at/v1/satellites/25544';
 async function getAPI_DATA_ISS() {
     const response = await fetch(api_url_iss);
     const data = await response.json();
-
-    // Deconstructing in JS (Separating lat and lon to individual Variable)
+    
+    // Destructuring in JS (Separating lat and lon to individual Variable)
     const {latitude, longitude} = data;
     // console.log(data);
     // console.log(latitude);
     // console.log(longitude);
 
+    // L.marker([latitude, longitude]).addTo(mymap);
+    marker.setLatLng([latitude, longitude]);
+    
     document.querySelector('#lat').textContent = latitude;
     document.querySelector('#lon').textContent = longitude;
-
+    
     // for (const x in data) {
-    //     console.log(`${x}: ${data[x]}`)
-    // }
-
-    // JS can't return multiple values, but can return an array thru object type
-    return { longitude, latitude };
-}
-
-getAPI_DATA_ISS().then(x => {
-    console.log(x.latitude);
-})
-
-
-
+        //     console.log(`${x}: ${data[x]}`)
+        // }
+        
+        // JS can't return multiple values, but can return an array thru object type
+        return { longitude, latitude };
+    }
+    
+    
+    getAPI_DATA_ISS().then(x => {
+        console.log(x.latitude);
+    })
+    
+    
 // Reloading the page every 5 sec
-// setInterval('window.location.reload()', 5000);
+setInterval(getAPI_DATA_ISS, 1000);
